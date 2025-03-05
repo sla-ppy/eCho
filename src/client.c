9fb0d6ef1d74304d35bebb5718f6a9b508f8a653
sla-ppy <sla-ppy-pm@protonmail.com>
2025-03-05 19:43:55 +0100

    code refactor after code review

--- src/client.c
+++ src/client.c
@@ -7,15 +7,9 @@
 #include <sys/socket.h>
 #include <netdb.h>
 
-void checkErrno() {
-    if (errno != 0) {  // we can check if errno is a non-zero value, errno always gets updated to the last error value
-        fprintf(stderr, "[CLIENT]: %s\n", strerror(errno));   // strerror converts errno values to human-readable for us!
-    }
-}
-
 int main(void) {
-    const char *node = "127.0.0.1";
-    const char *service = "10250"; // use above 1024 till 65535, if they aren't already used by some other program
+    const char *server_addr = "127.0.0.1";
+    const char *server_port = "10250"; // use above 1024 till 65535, if they aren't already used by some other program
 
     struct addrinfo hints;
     memset(&hints ,0, sizeof(hints)); // zero it
@@ -25,43 +19,39 @@ int main(void) {
     struct addrinfo *servinfo;
 
     // 0.
-    int status = getaddrinfo(node, service, &hints, &servinfo);
-    if (status != 0) {
-        fprintf(stderr, "getaddrinfo() error: %s\n", gai_strerror(status)); // convert getaddrinfo error to string
-        checkErrno();
+    int rc = getaddrinfo(server_addr, server_port, &hints, &servinfo);
+    if (rc != 0) {
+        fprintf(stderr, "getaddrinfo() error: %s\n", strerror(errno));
+        return 1;
     } else {
         printf("[CLIENT] Fetching address information successful.\n");
     }
 
     // 1.
-    int client_fd = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol);
-    if (client_fd == -1) {
-        fprintf(stderr, "socket() error: %s\n", gai_strerror(client_fd));
-        checkErrno();
+    int client_sfd = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol);
+    if (client_sfd == -1) {
+        fprintf(stderr, "socket() error: %s\n", strerror(errno));
+        return 1;
     } else {
         printf("[CLIENT] Connecting socket created.\n");
     }
 
-    bool transcieving = false;
     // 2.
-    int connect_rv = connect(client_fd, servinfo->ai_addr, servinfo->ai_addrlen);
-    if (connect_rv == -1) {
-        fprintf(stderr, "connect() error: %s\n", gai_strerror(connect_rv));
-        checkErrno();
+    rc = connect(client_sfd, servinfo->ai_addr, servinfo->ai_addrlen);
+    if (rc == -1) {
+        fprintf(stderr, "connect() error: %s\n", strerror(errno));
     } else {
-        printf("[CLIENT] Succesfully connected to %s:%s\n", node, service);
-        transcieving = true;
+        printf("[CLIENT] Succesfully connected to %s:%s\n", server_addr, server_port);
     }
 
     char buffer[512];
     int bytes_sent;
     int bytes_received = 1;
-    while (transcieving) {
+    while (true) {
         // 5. check if server wants to send anything first
-        bytes_received = recv(client_fd, buffer, sizeof(buffer), 0);
+        bytes_received = recv(client_sfd, buffer, sizeof(buffer), 0);
         if (bytes_received == -1) {
-            fprintf(stderr, "recv() error: %s\n", gai_strerror(client_fd));
-            checkErrno();
+            fprintf(stderr, "recv() error during transcieving: %s\n", strerror(errno));
         } else if (bytes_received == 0) {
             printf("[SERVER] Remote side has closed the connection\n");
             break;
@@ -72,15 +62,14 @@ int main(void) {
         printf("Enter message: ");
         fgets(buffer, 512, stdin);  // fgets() adds a terminating \0 at the end of the string
 
-        // 5. send any message we want
-        bytes_sent = send(client_fd, buffer, strlen(buffer)+1, 0); // strlen() by default excludes \0, so we +1
+        // 6. send any message we want
+        bytes_sent = send(client_sfd, buffer, strlen(buffer)+1, 0); // strlen() by default excludes \0, so we +1
         if (bytes_sent == -1) {
-            fprintf(stderr, "send() error: %s\n", gai_strerror(bytes_sent));
-            checkErrno();
+            fprintf(stderr, "send() error during transcieving: %s\n", strerror(errno));
         }
     }
 
-    close(client_fd);
+    close(client_sfd);
     freeaddrinfo(servinfo);
     return 0;
 }
--- src/server.c
+++ src/server.c
@@ -11,15 +11,9 @@
 
 #define BACKLOG 20
 
-void checkErrno(void) {
-    if (errno != 0) {  // we can check if errno is a non-zero value, errno always gets updated to the last error value
-        fprintf(stderr, "[ERROR]: %s\n", strerror(errno));   // strerror converts errno values to human-readable for us!
-    }
-}
-
 int main(void) {
-    const char *node = "127.0.0.1";
-    const char *service = "10250"; // use above 1024 till 65535, if they aren't already used by some other program
+    const char *server_addr = "127.0.0.1";
+    const char *server_port = "10250"; // use above 1024 till 65535, if they aren't already used by some other program
 
     struct addrinfo hints;
     struct addrinfo *servinfo;
@@ -28,109 +22,101 @@ int main(void) {
     hints.ai_socktype = SOCK_STREAM;
 
     // 0. fill servinfo
-    int status = getaddrinfo(node, service, &hints, &servinfo);
-    if (status != 0) {
-        fprintf(stderr, "getaddrinfo() error: %s\n", gai_strerror(status)); // convert getaddrinfo error to string
-        checkErrno();
+    int rc = getaddrinfo(server_addr, server_port, &hints, &servinfo);
+    if (rc != 0) {
+        fprintf(stderr, "getaddrinfo() error: %s\n", strerror(errno));
+        return 1;
     } else {
         printf("[SERVER] Fetching address information successful.\n");
     }
 
     // 1. create server socket
-    int listen_fd = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol);
-    if (listen_fd == -1) {
-        fprintf(stderr, "socket() error: %s\n", gai_strerror(listen_fd));
-        checkErrno();
+    int server_sfd = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol);
+    if (server_sfd == -1) {
+        fprintf(stderr, "socket() error: %s\n", strerror(errno));
+        return 1;
     } else {
         printf("[SERVER] Listening socket created.\n");
     }
 
     // 2. bind server socket to server port
-    int bind_rv = bind(listen_fd, servinfo->ai_addr, servinfo->ai_addrlen);
-    if (bind_rv == -1) {
-        fprintf(stderr, "bind() error: %s\n", gai_strerror(bind_rv));
-        checkErrno();
+    rc = bind(server_sfd, servinfo->ai_addr, servinfo->ai_addrlen);
+    if (rc == -1) {
+        fprintf(stderr, "bind() error: %s\n", strerror(errno));
+        return 1;
     } else {
-        printf("[SERVER] Succesfully bound to port: %s\n", service);
+        printf("[SERVER] Succesfully bound to port: %s\n", server_port);
     }
 
     // 3. start listening on port
-    bool listening = false;
-    int listen_rv = listen(listen_fd,BACKLOG); // 20 - backlog specifies how many connections are allowed on the incomming queue
-    if (listen_rv == -1) {
-        fprintf(stderr, "listen() error: %s\n", gai_strerror(bind_rv));
-        checkErrno();
+    rc = listen(server_sfd,BACKLOG); // 20 - backlog specifies how many connections are allowed on the incomming queue
+    if (rc == -1) {
+        fprintf(stderr, "listen() error: %s\n", strerror(errno));
+        return 1;
     } else {
-        printf("[SERVER] Listening on: %s:%s\n", node, service);
-        listening = true;
+        printf("[SERVER] Listening on: %s:%s\n", server_addr, server_port);
     }
 
     // client data
     socklen_t addr_size;
-    struct sockaddr_storage their_addr;
-    addr_size = sizeof their_addr;
+    struct sockaddr_storage client_addr;
+    addr_size = sizeof client_addr;
     char ip_addr[INET_ADDRSTRLEN];
-    int port;
+    int client_port;
 
     // server data
-    int comms_fd;
+    int client_sfd;
     int bytes_received;
     int bytes_sent;
     char buffer[512];
     char intro_msg[512] = "Welcome to eCho! Yell at me so I can yell back to you!\n";
-    bool transcieving = false;
-    while (listening) {
+    while (true) {
         // 4. We need two loops here so the server doesn't close after losing its client, this way client can reconnect, or in the future multiple clients can connect
-        comms_fd = accept(listen_fd, (struct sockaddr *)&their_addr, &addr_size);
-        if (comms_fd == -1) {
-            fprintf(stderr, "accept() error: %s\n", gai_strerror(bind_rv));
-            checkErrno();
-        } else {
-            // get client data
-            getpeername(comms_fd, (struct sockaddr*)&their_addr, &addr_size);
-            struct sockaddr_in *s = (struct sockaddr_in *)&their_addr;
-            port = ntohs(s->sin_port);
-            inet_ntop(AF_INET, &s->sin_addr, ip_addr, sizeof(ip_addr));
-            printf("[SERVER] Accepted connection: %s:%d\n", ip_addr, port);
-
-            // send welcome message to new connection
-            bytes_sent = send(comms_fd, intro_msg, strlen(intro_msg) + 1, 0);
-            if (bytes_sent == -1) {
-                fprintf(stderr, "send() error when sending intro msg: %s\n", gai_strerror(bytes_sent));
-                checkErrno();
-            }
-            transcieving = true;
+        client_sfd = accept(server_sfd, (struct sockaddr *)&client_addr, &addr_size);
+        if (client_sfd == -1) {
+            fprintf(stderr, "accept() error: %s\n", strerror(errno));
+            continue;
         }
 
-        while(transcieving) {
-            // 5. recv or send
-            bytes_received = recv(comms_fd, buffer, sizeof(buffer), 0);
+        // get client data
+        getpeername(client_sfd, (struct sockaddr *)&client_addr, &addr_size);
+        struct sockaddr_in *s = (struct sockaddr_in *)&client_addr;
+        client_port = ntohs(s->sin_port);
+        inet_ntop(AF_INET, &s->sin_addr, ip_addr, sizeof(ip_addr));
+        printf("[SERVER] Accepted connection: %s:%d\n", ip_addr, client_port);
+
+        // send welcome message to new connection
+        bytes_sent = send(client_sfd, intro_msg, strlen(intro_msg) + 1, 0);
+        if (bytes_sent == -1) {
+          fprintf(stderr, "send() error when sending intro msg: %s\n",
+                  strerror(errno));
+        }
+
+        while(true) {
+            // 5. check if we need to recv anything first
+            bytes_received = recv(client_sfd, buffer, sizeof(buffer), 0);
             if (bytes_received == -1) {
-                fprintf(stderr, "recv() error during transcieving: %s\n", gai_strerror(bind_rv));
-                checkErrno();
+                fprintf(stderr, "recv() error during transcieving: %s\n", strerror(errno));
             } else if (bytes_received == 0) {
                 printf("[SERVER] Remote side has closed the connection\n");
+                close(client_sfd);
                 break;
             } else {
                 printf("[CLIENT] %s", buffer);
             }
 
-            // 5. recv or send
-            bytes_sent = send(comms_fd, buffer, strlen(buffer) + 1, 0);
+            // 6. send the echo back
+            bytes_sent = send(client_sfd, buffer, strlen(buffer) + 1, 0);
             if (bytes_sent == -1) {
-                fprintf(stderr, "send() error during transcieving: %s\n", gai_strerror(bytes_sent));
-                checkErrno();
+                fprintf(stderr, "send() error during transcieving: %s\n", strerror(errno));
             }
 
-            // 6. clear buffer, get ready to recv() next msg
-            for (int i = 0; i != sizeof(buffer); i++) {
-                buffer[i] = '\0';
-            }
+            // 7. clear buffer, get ready to recv() next msg
+            memset(buffer, 0, sizeof(buffer));
         }
     }
 
-    close(comms_fd);
-    close(listen_fd);
+    close(server_sfd);
     freeaddrinfo(servinfo);
     return 0;
 }
